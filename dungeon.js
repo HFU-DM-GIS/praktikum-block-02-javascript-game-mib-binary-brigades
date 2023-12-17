bbec250604a81fadad835f87b8cd7d82facd86aa
croc001 <croc12357@gmail.com>
2023-12-17 01:09:55 +0100

    room Gen
    added Room gen

--- dungeon.js
+++ dungeon.js
@@ -3,8 +3,8 @@ let canvasContext = canvas.getContext("2d"); // makes the canvas 2d
 
 let widthOfTiles  = 20; // dimension of Tiles
 
-let rows = 5;
-let cols = 5;
+let rows = 50;
+let cols = 50;
 
 let grid = []; //stores the Layout
 
@@ -12,8 +12,8 @@ let rooms = []; // stores the Rooms
 let collide = false; //Sets Rooms collide false
 
 let amountOfRooms = 7;
-let RoomSizeMax = 5;
-let RoomSizeMin = 5
+let size = 5;
+let roomSizeMin = 5
 
 let disX;
 let disY;
@@ -21,9 +21,9 @@ let corridorWidth = 1;
 
 //--------------------------------------------------------------//
 
-class Cell
+class Cell // cell Object
 {
-    constructor(col, row, x, y )// cell Object
+    constructor(col, row, x, y )
     {
         this.col = col;
         this.row = row;
@@ -51,20 +51,20 @@ class Cell
 
     carveRooms(dis, x, y) //carve out Rooms
     {
-        for (let i = 0; i < rooms.length; i++)
-        {
-            if (this.col >= rooms[i].y  / widthOfTiles && this.col < rooms[i].y / widthOfTiles + rooms[i].h / widthOfTiles && this.row >= rooms[i].x / widthOfTiles && this.row < rooms[i].x / widthOfTiles + rooms[i].widthOfTiles/ widthOfTiles)
-                    {
-                    this.empty = true;
-                    }
-        }
+        for (let i = 0; i < rooms.length; i++) 
+					{
+						if(this.col >= rooms[i].y / widthOfTiles && this.col < rooms[i].y / widthOfTiles+rooms[i].h / widthOfTiles && this.row >= rooms[i].x /widthOfTiles && this.row < rooms[i].x/ widthOfTiles +rooms[i].w / widthOfTiles)
+							{
+								this.empty = false;
+							}
+					}
     };
 
      carveH(dis, x, y) //carve out the horizontal corridors
     {
         if (this.row >= y && this.row < y + dis && this.col < corridorWidth && this.col > y - corridorWidth)
         {
-            this.empty = true;
+            this.empty = false;
         }
     };
 
@@ -72,7 +72,7 @@ class Cell
     {
         if (this.col >= y && this.col < y +dis && this.row < x + corridorWidth && this.row > x - corridorWidth)
         {
-            this.empty = true;
+            this.empty = false;
         }
     };
 }
@@ -88,15 +88,47 @@ function makeGrid()
             let cell = new Cell(c, r, x, y)
             grid.push(cell);
         }
+
     }
 }
+//--------------------------------------------------------------//
+class Room
+{
+    constructor (x, y, width, height, i)
+    {
+        this.x = (x - 1) * widthOfTiles;
+        this.y = (y - 1) * widthOfTiles;
+        this.w = width * widthOfTiles;
+        this.h = height * widthOfTiles;
+
+        this.center = [
+            Math.floor(this.x / widthOfTiles + width / 2),
+            Math.floor(this.y / widthOfTiles + height / 2)
+        ]
+    } 
+
+    draw() 
+    {
+        canvasContext.fillStyle = "white";
+        canvasContext.fillText(i, this.x + this.w / 2, this.y+this.h / 2-20)
+    }
+}
+
+function createRooms() {
+    for (let i = 0; i < amountOfRooms; i++)
+    {
+     let room = new Room(Math.floor(Math.random() * rows) + 1, Math.floor(Math.random() * cols) + 1, Math.floor(Math.random() * size) + roomSizeMin, Math.floor(Math.random() * size) + roomSizeMin, i)
+     rooms.push(room)
+    }
+}
+//--------------------------------------------------------------//
 
 function draw()
 {
     for (let i = 0; i < grid.length; i++)
     {
-        grid[i].show();
         grid[i].carveRooms();
+        grid[i].show();
     }
 
     for (let i = 0; i < grid.rooms; i++)
@@ -106,5 +138,6 @@ function draw()
 }
 
 makeGrid();
-// createRooms(); not implemented yet
+createRooms();
+console.log(rooms[1].w)
 draw();
