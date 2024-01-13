880a0c3e9784ca47e39da1c0feb162c590d1465f
pascalgeist <127938585+pascalgeist@users.noreply.github.com>
2024-01-13 19:26:00 +0100

    Creating new html files
    adding startpage, update player.js and style.css

--- player.js
+++ player.js
@@ -9,6 +9,9 @@ let keys = [];
 let keyX = 100;
 let keyY = 100;
 let keySize = 20;
+let doorX = 2000;
+let doorY = 2000;
+let doorSize = 20;
 let score = 0;
 let quotes = [];
 
@@ -53,9 +56,19 @@ function update() { //player movement/score
         playerX = playerX + speed
     }
     // dieses if statement in einer Funktion kapseln und diese sinnvoll benennen
+   
     if (playerX + playerSize > keyX && playerY + playerSize > keyY && keyX + keySize > playerX && keyY + keySize > playerY) {
-        playerCollectedKey()
+        getDoor();
     }
+    if (playerX + playerSize > doorX && playerY + playerSize > doorY && doorX + doorSize > playerX && doorY + doorSize > playerY) {
+        openDoor();
+    }
+    /*
+    if (playerX + playerSize > Room.X && playerY + playerSize > Room.Y && Room.X + Room.Size > playerX && Room.Y + Room.Size > playerY) {
+        boxcollision()
+    }
+    */
+
     // hier holt ihr euch im loop immer wieder einen neuen Spruch, das führt nach einer Weile zu einem Fehler
     if (score > 0 && score % 1 == 0) {
         fetchQuote().then(quote => {
@@ -64,13 +77,26 @@ function update() { //player movement/score
     }
 
 }
-
-
-function playerCollectedKey() { // benennt die Funktion nach dem was sie tutm nicht wann sie aufgerufen wird.
-    keyX = Math.random() * (600 - keySize);
-    keyY = Math.random() * (600 - keySize);
-    score = score + 1;
+function boxcollision(){
+    playerX = + 50;
+    playerY = + 50;
 }
+
+function getDoor() { // benennt die Funktion nach dem was sie tutm nicht wann sie aufgerufen wird.
+    doorX = Math.random() * (600 - keySize);
+    doorY = Math.random() * (600 - keySize);
+    keyX = 2000;
+    keyY = 2000;
+    
+}
+function openDoor(){
+    score = score + 1;
+    doorX = 2000;
+    doorY = 2000;
+    //
+}
+
+
 function render() {      //creating viewable objects
     context.clearRect(0, 0, 1000, 1000)
     context.fillStyle = playercolor     //player
@@ -80,6 +106,12 @@ function render() {      //creating viewable objects
     context.beginPath()
     context.arc(keyX + keySize / 2, keyY + keySize / 2, keySize / 2, keySize / 2, 0, 360)
     context.fill()
+
+    context.fillStyle = "green"    //door
+    context.beginPath()
+    context.arc(doorX + doorSize / 2, doorY + doorSize / 2, doorSize / 2, doorSize / 2, 0, 360)
+    context.fill()
+
 
     context.fillStyle = "white"     //score
     context.font = "20px Impact"
@@ -94,5 +126,6 @@ function render() {      //creating viewable objects
 function clearPlayer() {
     drawPlayer(player.initialX, player.initialY, player.size);
 }
+init()
 window.setInterval(loop, 1000 / 60)
-init() // ungewöhnlich die Initialisierung nach dem Starten des Game Loops zu machen
+
